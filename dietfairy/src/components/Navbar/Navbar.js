013e35ba56206b81d3e26ccbbd88463835604afd
coltonfalkner <coltonfalkner@gmail.com>
2020-03-21 12:50:56 -0500

    Navbar component initial

---
+++ dietfairy/src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            //home button to be set to home link
+            <a className="nav-item nav-link active" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            //saved href to be set to final link
+            <a className="nav-item nav-link" href="#">
+              Saved
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
