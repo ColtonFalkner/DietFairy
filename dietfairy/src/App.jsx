b5a21dee52fcc11331ce6f815b3a0cdd63c734a4
coltonfalkner <coltonfalkner@gmail.com>
2020-03-31 23:52:48 -0500

    removed some pointers

--- dietfairy/src/App.jsx
+++ dietfairy/src/App.jsx
@@ -74,9 +74,7 @@ class App extends Component {
     const recipeName = e.target.elements.recipe.value;
     const from = Math.floor(Math.random() * 98);
     const to = from + 6;
-    const url = `https://api.edamam.com/search?q=${recipeName}&app_id=${
-      keys.APP_ID
-    }&app_key=${keys.APP_KEY}&from=${from.toString()}&to=${to.toString()}`;
+    const url = `https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${from.toString()}&to=${to.toString()}`;
     try {
       let response = await fetch(url);
       if (!response.ok) {
