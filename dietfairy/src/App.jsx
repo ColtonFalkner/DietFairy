aa524f776f9c362ddb72a85180bfb4ad5900aca7
coltonfalkner <coltonfalkner@gmail.com>
2020-04-01 00:31:08 -0500

    reversion to local for edits

--- dietfairy/src/App.jsx
+++ dietfairy/src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./components/Home";
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
-// import keys from "./config/keys";
+import keys from "./config/keys";
 
 const DisplayLinks = props => {
   if (props.loggedIn) {
@@ -75,10 +75,8 @@ class App extends Component {
     const from = Math.floor(Math.random() * 98);
     const to = from + 6;
     const url = `https://api.edamam.com/search?q=${recipeName}&app_id=${
-      process.env.APP_ID
-    }&app_key=${
-      process.env.APP_KEY
-    }&from=${from.toString()}&to=${to.toString()}`;
+      keys.APP_ID
+    }&app_key=${keys.APP_KEY}&from=${from.toString()}&to=${to.toString()}`;
     try {
       let response = await fetch(url);
       if (!response.ok) {
@@ -92,7 +90,7 @@ class App extends Component {
   };
   //----------------------------
   componentDidMount() {
-    axios.get("https://diet-fairy.herokuapp.com/auth/user").then(response => {
+    axios.get("http://localhost:3000/auth/user").then(response => {
       console.log(response);
       if (!!response.data.user) {
         // console.log('THERE IS A USER')
@@ -125,7 +123,7 @@ class App extends Component {
 
   _login(username, password) {
     axios
-      .post("https://diet-fairy.herokuapp.com/auth/login", {
+      .post("http://localhost:3000/auth/login", {
         username,
         password
       })
